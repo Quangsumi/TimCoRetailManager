9c59cc74a4775860792abc2f355614b52283ecce
Quangsumi <djquangsumi@gmail.com>
2019-11-17 16:22:44 +0700

    Add Remove From Cart Functionality

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -62,6 +62,20 @@ namespace TRMDesktopUI.ViewModels
             { 
                 _selectedProduct = value;
                 NotifyOfPropertyChange(() => SelectedProduct); 
+                NotifyOfPropertyChange(() => CanAddToCart); 
+            }
+        }
+
+        private CartItemDisplayModel _selectedCartItem;
+
+        public CartItemDisplayModel SelectedCartItem
+        {
+            get { return _selectedCartItem; }
+            set
+            {
+                _selectedCartItem = value;
+                NotifyOfPropertyChange(() => SelectedCartItem);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
             }
         }
 
@@ -76,7 +90,6 @@ namespace TRMDesktopUI.ViewModels
                 NotifyOfPropertyChange(() => ItemQuantity);
                 NotifyOfPropertyChange(() => CanAddToCart);
             }
-
         }
 
         private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
@@ -190,7 +203,10 @@ namespace TRMDesktopUI.ViewModels
                 bool output = false;
 
                 // Make sure something is selected
-                // Make sure there is an item quantity
+                if (SelectedCartItem != null && SelectedCartItem?.Product.QuantityInStock > 0)
+                {
+                    output = true;
+                }
 
                 return output;
             }
@@ -198,6 +214,12 @@ namespace TRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
+            SelectedCartItem.Product.QuantityInStock += 1;
+
+            if (SelectedCartItem.QuantityInCart > 1)
+                SelectedCartItem.QuantityInCart -= 1;
+            else
+                Cart.Remove(SelectedCartItem);
 
             NotifyOfPropertyChange(() => SubTotal); 
             NotifyOfPropertyChange(() => Tax);
